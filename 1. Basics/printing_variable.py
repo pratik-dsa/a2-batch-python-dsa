6aebab34b526eb3adbf8cf51245b9e590139797c
pratik.dev <jalanpratik2@gmail.com>
2024-02-29 10:37:23 +0530

    Initial commit

--- 1. Basics/printing_variable.py
+++ 1. Basics/printing_variable.py
@@ -9,7 +9,7 @@ gender = "Male"
 # print("My name is", name, "my age is", age, "my gender is", gender)
 
 # Method 2 (F-Strings)
-# print(f"My name is {name}")
+print(f"My name is {name}")
 # print(f"My age is {age}")
 # print(f"My gender is {gender}")
 # print(f"My name is {name} and my age is {age}, my gender is {gender}")
---
+++ 1. Basics/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(f"My name is {name}")
