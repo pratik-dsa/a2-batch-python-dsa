ca65c523dec3876169de32339459c16e4a77bd63
pratik.dev <jalanpratik2@gmail.com>
2024-02-29 11:08:13 +0530

    changes in basics

--- 1. Basics/annotations.py
+++ 1. Basics/annotations.py
@@ -1,14 +1,19 @@
-a: int = 5
-print(a)
+# a: int = 5
+# print(a)
 
-a = 432
-print(a)
+# a = 432
+# print(a)
 
-b: int | float | str = 55
-print(b)
+# b: int | float | str = 55
+# print(b)
 
-b = 55.67
-print(b)
+# b = 55.67
+# print(b)
+
+c: int | float = (
+    "pratik"  # it contains annotattion but it will doesnt give, just it will give warning.
+)  # type: ignore
+print(c)
 
 
-r: int | float = 10 / 5
+# r: int | float = 10 / 5
